35b4f1682f326e1a72d1bcf7602c8c9224b7f7d2
Lokendra Singh <lokendrasolanki60@gmail.com>
2024-05-20 03:44:34 +0530

    Create lead_lag.sql

---
+++ lead_lag.sql
@@ -0,0 +1,47 @@
+-- create sales table
+CREATE TABLE sales (
+    product VARCHAR(50),
+    year INT,
+    sales_amount DECIMAL(10,2)
+);
+
+-- Add some dummy data into the table
+INSERT INTO sales (product, year, sales_amount) VALUES
+('Apples', 2021, 1500.50),
+('Bananas', 2021, 2500.75),
+('Carrots', 2021, 3200.00),
+('Apples', 2022, 1700.30),
+('Bananas', 2022, 2900.20),
+('Carrots', 2022, 3400.60),
+('Apples', 2023, 1800.00),
+('Bananas', 2023, 3100.45),
+('Carrots', 2023, 3600.80),
+('Oranges', 2021, 1100.25),
+('Oranges', 2022, 1300.50),
+('Oranges', 2023, 1400.75),
+('Tomatoes', 2021, 1200.00),
+('Tomatoes', 2022, 1500.35),
+('Tomatoes', 2023, 1600.90);
+
+-- LEAD Example
+SELECT
+    product,
+    year,
+    sales_amount,
+    LEAD(sales_amount, 1, 0) OVER (
+        ORDER BY year, product
+    ) AS next_product_sales
+FROM
+    sales ;
+
+
+-- LAG Example
+SELECT
+    product,
+    year,
+    sales_amount,
+    LAG(sales_amount, 1, 0) OVER (
+        ORDER BY year, product
+    ) AS prev_product_sales
+FROM
+    sales ;
